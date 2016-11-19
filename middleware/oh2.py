fa759967d25d91aa42937947a160737786cb63a9
lstrz <->
2016-11-19 12:45:32 +0100

    Added comments and error handling.

--- middleware/oh2.py
+++ middleware/oh2.py
@@ -5,32 +5,72 @@ addr = "http://127.0.0.1:8080"
 rest_path = "/rest"
 items_path = rest_path + "/items"
 
+# Sets a new address to send requests to. Provide an IP/domain with a port, without the protocol.
+# Protocol gets automatically added. Does not return anything. Default is 127.0.0.1,
+# Example: SetAddr("192.168.0.22:1337").
 def SetAddr(new_addr):
   global addr
   addr = "http://" + new_addr
 
-def GetRequest(url):
-  return json.loads(urllib.request.urlopen(url).read().decode("utf-8"))
+# Sends a GET HTTP request to OpenHab on the given URL.
+# If all goes well, returns whatever OpenHAB returned, interpreted as JSON.
+# If something goes wrong, returns {"error":400} (but using the real code that it got).
+def _GetRequest(url):
+  try:
+    res = urllib.request.urlopen(url)
+  except urllib.error.HTTPError as err:
+    return {"error":err.code}
+  else:
+    return json.loads(res.read().decode("utf-8"))
 
-def PostRequest(url, data):
+# Sends a POST HTTP request to OpenHAB on the given URL with the given data in the body.
+# If all goes well returns {}.
+# If something goes wrong, returns {"error":400} (but using the real code that it got).
+def _PostRequest(url, data):
   req = urllib.request.Request(url, data.encode(), {"Content-Type": "text/plain"})
-  urllib.request.urlopen(req)
+  try:
+    res = urllib.request.urlopen(req)
+  except urllib.error.HTTPError as err:
+    return {"error":err.code}
+  else:
+    return {}
 
+# Gets all items from OpenHAB. 
+# If all goes well returns [{...}. {...}, ...], where each {...} is an item as in the
+# GetItem(item) method.
+# If something goes wrong, returns {"error":400} (but using the real code that it got).
 def GetItems():
   global addr
   global items_path
   url = addr + items_path
-  return GetRequest(url)
+  return _GetRequest(url)
 
+# Gets an item from OpenHAB. 
+# If all goes well returns something like
+# {
+#  "link": "http://localhost:8080/rest/items/l1",
+#  "state": "100",
+#  "type": "Dimmer",
+#  "name": "l1",
+#  "label": "Light 1",
+#  "tags": [],
+#  "groupNames": [
+#    "sg1"
+#  ]
+# }
+# If something goes wrong, returns {"error":400} (but using the real code that it got).
 def GetItem(item):
   global addr
   global items_path
   url = addr + items_path + "/" + item
-  return GetRequest(url)
+  return _GetRequest(url)
 
+# Sends a command to OpenHAB for the specifid item.
+# If all goes well returns {}.
+# If something goes wrong, returns {"error":400} (but using the real code that it got).
 def SetItem(item, command):
   global addr
   global items_path
   url = addr + items_path + "/" + item
-  PostRequest(url, command)
+  return _PostRequest(url, command)
 
