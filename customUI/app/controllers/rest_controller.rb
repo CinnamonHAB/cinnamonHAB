33736f046071a115251cc24877dae2726ea8b009
Fabio Rodrigues <fjdrodrigues@hotmail.com>
2016-12-20 00:39:29 +0100

    Custom UI - Devices Listing

--- customUI/app/controllers/rest_controller.rb
+++ customUI/app/controllers/rest_controller.rb
@@ -7,29 +7,10 @@ class RestController < ApplicationController
 	res = Net::HTTP.start(url.host, url.port) {|http|
 	  http.request(req)
 	}
-  	itemArray = res.body.split('},{')
-  	tempArray = Array.new(itemArray.length()-1)
-  	finalArray = Array.new(itemArray.length()-1)
-  	i = 1
-  	while i < itemArray.length() do
-  		tempArray[i-1] = itemArray[i].split('","')
-  		item = Array.new(2) #type, name
-  		j = 0
-  		while j < tempArray[i-1].length() do
-			temp = tempArray[i-1][j].split('":"')
-			if temp[0] == "type"
-				item[0] = temp[1]
-			end
-			if temp[0] == "name"
-				item[1] = temp[1]
-			end
-			j += 1
-  		end
-  		finalArray[i-1] = item
-  		i += 1
-  	end
+  render plain: res.body
+	end
   	
-  	render json: res.body
+  	
   end
   def openhab_sendcommand
   	data = "#{request.body.read}"
