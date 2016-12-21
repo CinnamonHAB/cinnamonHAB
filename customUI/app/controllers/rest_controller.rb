ee34fa6c71b9b50f0753d549a5636259782be1fd
Fabio Rodrigues <fjdrodrigues@hotmail.com>
2016-12-21 01:49:37 +0100

    Custom UI - Update

--- customUI/app/controllers/rest_controller.rb
+++ customUI/app/controllers/rest_controller.rb
@@ -2,26 +2,30 @@ class RestController < ApplicationController
   def openhab_getitems
   	require 'net/http'
 
-	url = URI.parse('http://localhost/rest/items')
-	req = Net::HTTP::Get.new(url.to_s)
-	res = Net::HTTP.start(url.host, url.port) {|http|
-	  http.request(req)
-	}
-  render json: res.body
+  	uri = URI.parse('http://localhost/rest/items')
+  	req = Net::HTTP::Get.new(uri.to_s)
+  	res = Net::HTTP.start(uri.host, uri.port) {|http|
+  	  http.request(req)
+  	}
+    render json: res.body
 	end
   def openhab_sendcommand
-  	data = "#{request.body.read}"
-  	
+    require 'net/http'
+    require 'uri'
+
+  	data = "#{request.body.read}"  	
   	dataArray = data.split(' ')
   	item = dataArray[0]
   	command = dataArray[1]
-  	require 'net/http'
-
-	url = URI.parse("http://localhost/rest/items/"+item)
-	http = Net::HTTP.new(url.host, url.port)
-	req = Net::HTTP::POST.new(url.path, {'Content-Type' => 'text/plain'})
-	req.body = command
-	res = http.request(req)
-  render text: res
+  	header = { 'Content-Type': 'text/plain'}
+  	uri = URI.parse('http://localhost/rest/items/'+item)
+  	req = Net::HTTP::Post.new(uri.to_s, header)
+  	req.body = command
+    
+    res = Net::HTTP.start(uri.host, uri.port) {|http|
+      http.request(req)
+    }
+    puts res.code
+    render plain: res
   end
 end
