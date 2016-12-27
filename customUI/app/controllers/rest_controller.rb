799cba9c14644567b9e8b58808a295f75d4f51d0
opalmgren <oskar.palmgren@gmail.com>
2016-12-27 20:00:10 +0100

    added functions to communicate with openhab to the custom ui.

--- customUI/app/controllers/rest_controller.rb
+++ customUI/app/controllers/rest_controller.rb
@@ -53,4 +53,126 @@ class RestController < ApplicationController
     }
     render plain: res.code
   end
+  
+  def openhab_addItem
+    require 'net/http'
+    require 'uri'
+
+    data = "#{request.body.read}" 
+    
+    uri = URI.parse('http://localhost/rest/items/')
+    req = Net::HTTP::Put.new(uri.to_s,{'Content-Type' => 'text/plain'})
+    req.body = data
+    res = Net::HTTP.start(uri.host, uri.port) {|http|
+      http.request(req)
+    }
+    render plain: res.code
+  end
+
+  def openhab_deleteItem
+    require 'net/http'
+    require 'uri'
+    
+    data = "#{request.body.read}" 
+    dataArray = data.split(' ')
+    item = dataArray[0]
+    
+    uri = URI.parse('http://localhost/rest/items/'+item+'/')
+    
+    req = Net::HTTP::Delete.new(uri.to_s)
+    
+    res = Net::HTTP.start(uri.host, uri.port) {|http|
+      http.request(req)
+    }
+    render plain: res.code
+  end
+
+  def openhab_getItem
+    require 'net/http'
+    
+    data = "#{request.body.read}" 
+    dataArray = data.split(' ')
+    item = dataArray[0]
+    
+  	uri = URI.parse('http://localhost/rest/items'+item+'/')
+  	req = Net::HTTP::Get.new(uri.to_s)
+  	res = Net::HTTP.start(uri.host, uri.port) {|http|
+  	  http.request(req)
+  	}
+    render json: res.body
+  end
+
+  def openhab_addItemToGroup
+    require 'net/http'
+    require 'uri'
+
+    data = "#{request.body.read}" 
+    dataArray = data.split(' ')
+    groupName = dataArray[0]
+    memberName = dataArray[1]
+    
+    uri = URI.parse('http://localhost/rest/items/'+groupName+'/')
+    req = Net::HTTP::Put.new(uri.to_s,{'Content-Type' => 'text/plain'})
+    req.body = memberName
+    res = Net::HTTP.start(uri.host, uri.port) {|http|
+      http.request(req)
+    }
+    render plain: res.code
+ end
+
+ def openhab_removeItemFromGroup
+   require 'net/http'
+   require 'uri'
+    
+   data = "#{request.body.read}" 
+   dataArray = data.split(' ')
+   groupName = dataArray[0]
+   item = dataArray[1]
+    
+   uri = URI.parse('http://localhost/rest/items/'+groupName+'/'+item+'/')
+    
+   req = Net::HTTP::Delete.new(uri.to_s)
+    
+   res = Net::HTTP.start(uri.host, uri.port) {|http|
+     http.request(req)
+   }
+   render plain: res.code
+ end
+
+ def openhab_addTagToItem
+   require 'net/http'
+   require 'uri'
+
+   data = "#{request.body.read}" 
+   dataArray = data.split(' ')
+   item = dataArray[0]
+   tag = dataArray[1]
+        
+   uri = URI.parse('http://localhost/rest/items/'+item+'/')
+   req = Net::HTTP::Put.new(uri.to_s,{'Content-Type' => 'text/plain'})
+   req.body = tag
+   res = Net::HTTP.start(uri.host, uri.port) {|http|
+     http.request(req)
+   }
+   render plain: res.code
+ end
+
+ def openhab_removeTagFromItem
+   require 'net/http'
+   require 'uri'
+    
+   data = "#{request.body.read}" 
+   dataArray = data.split(' ')
+   item = dataArray[0]
+   tag = dataArray[1]
+    
+   uri = URI.parse('http://localhost/rest/items/'+item+'/'+tag+'/')
+    
+   req = Net::HTTP::Delete.new(uri.to_s)
+    
+   res = Net::HTTP.start(uri.host, uri.port) {|http|
+     http.request(req)
+   }
+   render plain: res.code
+ end
 end
--- customUI/config/routes.rb
+++ customUI/config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
   get 'rest/openhab_getitems'
+  get 'rest/openhab_getItem'
   get 'main/application'
   root 'main#application'
   match '/rest/getitems' => 'rest#openhab_getitems', via: :get
   match '/rest/getstate' => 'rest#openhab_getstate', via: :get
   match '/rest/updatestate' => 'rest#openhab_updatestate', via: :put
   match '/rest/sendcommand' => 'rest#openhab_sendcommand', via: :post
+  match '/rest/addItem' => 'rest#openhab_addItem', via: :put
+  match '/rest/deleteItem' => 'rest#openhab_deleteItem', via: :delete
+  match '/rest/getItem' => 'rest#openhab_getItem', via: :get
+  match '/rest/addItemToGroup' => 'rest#openhab_addItemToGroup', via: :put
+  match '/rest/removeItemFromGroup' => 'rest#openhab_removeItemFromGroup', via: :delete
+  match '/rest/addTagToItem' => 'rest#openhab_addTagToItem', via: :put
+  match '/rest/removeTagFromItem' => 'rest#openhab_removeTagFromItem', via: :delete
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
