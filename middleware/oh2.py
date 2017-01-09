023b9f91faade2f2577b246072a85a40d763cc39
lstrz <lstrz@users.noreply.github.com>
2017-01-09 21:52:17 +0100

    Echo middleware server.

--- middleware/oh2.py
+++ middleware/oh2.py
@@ -263,10 +263,10 @@ class Stream(http.server.BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
+        request = self.rfile.read().decode("utf8")
         # Send message back to client
-        message = "Hello world!"
         # Write content as utf-8 data
-        self.wfile.write(bytes(message, "utf8"))
+        self.wfile.write(bytes(request, "utf8"))
         return
 
 
